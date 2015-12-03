f5f50c0f8be8277148e1574a1b857bba4ed2898d
Chris Name <l018528b@student.staffs.ac.uk>
2015-12-03 20:40:26 +0000

    Updated the EP_Game.h and .cpp to interact with the leaderboard. Game scene now temporariliy transitions from game scene to leaderboard scene once count reaches 500.

--- Classes/EP_Game.cpp
+++ Classes/EP_Game.cpp
@@ -1,42 +1,49 @@
+//INCLUDES
 #include "EP_Game.h"
-
+//=========================================//
 cocos2d::Scene* EP_Game::createScene()
 {
 	// 'scene' is an autorelease object
 	auto scene = cocos2d::Scene::create();
-
 	// 'layer' is an autorelease object
 	auto layer = EP_Game::create();
-
 	// add layer as a child to scene
 	scene->addChild(layer);
-
+	//RETURN
 	return scene;
 }
-
+//=========================================//
 bool EP_Game::init()
 {
+	//A call to super? C++ doesn't have one so this is needed.
 	if (!Layer::init())
+	{
 		return false;
-
+	}
+	//Scene Setter
 	_rootNode = cocos2d::CSLoader::createNode("GameScene.csb");
 	this->addChild(_rootNode);
-
 	// get components from level
 	_backdrop = (cocos2d::Sprite*)_rootNode->getChildByName("GameBoardScreen_1");
 	_frameCounter = (cocos2d::ui::Text*)_rootNode->getChildByName("Frame_Counter");
-
+	//Random Generator
 	srand(time(NULL));
+	//Get the view pane size
 	_winSize = cocos2d::Director::getInstance()->getVisibleSize();
+	
+	
 	//_gameManager = GameManager::GetInstance();
+	
+	
 	_running = true;
 	_frameCount = 0;
 
+	//Calls the game loop
 	this->scheduleUpdate();
-
+	//INIT ENDS IN RETURN TRUE
 	return true;
 }
-
+//=========================================//
 void EP_Game::update(float deltaTime)
 {
 	if (_running)
@@ -44,8 +51,11 @@ void EP_Game::update(float deltaTime)
 		// Update the game
 		_frameCount += 1;
 
-		if (_frameCount >= 1000)
-			_frameCount = 0;
+		//TRANSITION TEST 
+		if (_frameCount >= 500)
+		{
+			EndGame();
+		}
 
 		std::ostringstream convert;
 		convert << _frameCount;
@@ -54,6 +64,13 @@ void EP_Game::update(float deltaTime)
 	}
 	if (_gameOver)
 	{
-		// Load leaderboard
+		//EndGame();
 	}
+}
+//=========================================//
+void EP_Game::EndGame()
+{
+	//Transitions from the game scene to the leaderboard scene
+	cocos2d::Scene* nextScene = LeaderBoard::createScene();
+	cocos2d::CCDirector::getInstance()->replaceScene(nextScene);
 }
--- Classes/EP_Game.h
+++ Classes/EP_Game.h
@@ -2,17 +2,19 @@
 #ifndef __EP_GAME_H__
 #define __EP_GAME_H__
 #define ELF_NUMBER 11
-
+//=========================================//
+//INCLUDES
 #include "cocos2d.h"
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
-#include "GameManager.h"
-#include "Elf.h"
 #include <string.h>
 #include <sstream>
-
+#include "GameManager.h"
+#include "Elf.h"
+#include "LeaderBoard.h"
+//=========================================//
 using namespace std;
-
+//=========================================//
 class EP_Game : public cocos2d::Layer
 {
 private:
@@ -31,10 +33,15 @@ private:
 	cocos2d::Size _winSize;
 	cocos2d::Node* _rootNode;
 public:
+	//Implements the "createScene();"
 	CREATE_FUNC(EP_Game);
+	//Sets up the Leaderboard scene
 	static cocos2d::Scene* createScene();
+	//Basically a cocos constructor
 	virtual bool init();
+	//Custom Methods
 	void update(float);
+	void EndGame();
 };
 
-#endif
+#endif //__EP_GAME_H__
